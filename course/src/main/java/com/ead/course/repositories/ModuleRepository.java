f899a1d9b0bbb031fd4588d2aebad9546e290840
Vinicius Kaminski <vikaminski@hotmail.com>
2023-02-06 15:21:10 -0300

    feat: Add fetch, type lazy and query to repository

--- course/src/main/java/com/ead/course/repositories/ModuleRepository.java
+++ course/src/main/java/com/ead/course/repositories/ModuleRepository.java
@@ -1,9 +1,18 @@
 package com.ead.course.repositories;
 
 import com.ead.course.models.CourseModel;
+import com.ead.course.models.ModuleModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface ModuleRepository extends JpaRepository<CourseModel, UUID> {
+
+    @Modifying
+    @Query(value="select * from tb_modules where course_course_id = :courseId", nativeQuery = true)
+    List<ModuleModel> findAllModulesIntoCurse(@Param("courseId") UUID courseId);
 }
